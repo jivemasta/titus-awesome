09ebcd704d3e2dfdc09aa1179acfddaf9772bdc9
TJ <jivemasta@gmail.com>
2021-12-02 19:39:44 -0500

    stuff

--- configuration/apps.lua
+++ configuration/apps.lua
@@ -26,13 +26,10 @@ return {
   run_on_start_up = {
     'picom --config ' .. filesystem.get_configuration_dir() .. '/configuration/picom.conf',
     'nm-applet --indicator', -- wifi
-    'pnmixer', -- shows an audiocontrol applet in systray when installed.
+    'volumeicon' --Mixer icon in the systray
     --'blueberry-tray', -- Bluetooth tray icon
     'numlockx on', -- enable numlock
-    '/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 & eval $(gnome-keyring-daemon -s --components=pkcs11,secrets,ssh,gpg)', -- credential manager
     'xfce4-power-manager', -- Power manager
-    'synology-drive -minimized',
-    'steam -silent',
     'feh --randomize --bg-fill ~/.wallpapers/*',
     '/usr/bin/variety',
     -- Add applications that need to be killed between reloads
